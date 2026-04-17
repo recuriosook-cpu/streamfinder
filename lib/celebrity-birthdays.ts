819d28ebb151677113d8c4ce7791023e45aef6e6
recuriosook-cpu <recuriosook-cpu@users.noreply.github.com>
2026-04-16 22:56:38 -0300

    fix: Anya Taylor-Joy correct ID

--- lib/celebrity-birthdays.ts
+++ lib/celebrity-birthdays.ts
@@ -1,5 +1,5 @@
 export const CELEBRITY_BIRTHDAYS = [
-  { id: 1340062, name: 'Anya Taylor-Joy', birthday: '1996-04-16', popularity: 93 },
+  { id: 1397778, name: 'Anya Taylor-Joy', birthday: '1996-04-16', popularity: 93 },
   { id: 1590797, name: 'Sadie Sink', birthday: '2002-04-16', popularity: 85 },
   { id: 78029, name: 'Martin Lawrence', birthday: '1965-04-16', popularity: 78 },
   { id: 10515, name: 'Charlie Chaplin', birthday: '1889-04-16', popularity: 72 },
